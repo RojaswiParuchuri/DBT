93f30b7cf74bb8af7e0dd7d67fb366cbe4b250bc
rojaswiparuchuri123@gmail.com <rojaswiparuchuri123@gmail.com>
2026-06-04 09:31:17 +0000

    explored datafreshness

--- models/staging/stg_jaffle_shop__customers.sql
+++ models/staging/stg_jaffle_shop__customers.sql
@@ -1,1 +1,1 @@
-select id as customer_id, first_name, last_name from {{ source('jaffle_shop', 'CUSTOMERS') }}
+select id as customer_id, first_name, last_name from {{ source('jaffle_shop', 'customers') }}
